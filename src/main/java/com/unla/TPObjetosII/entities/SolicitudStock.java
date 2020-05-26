ba78992d2558685296031b2d6a8033e15bcd83b6
enzord07 <enzord07@gmail.com>
2020-05-26 02:13:24 -0300

    Alta de solicitud con local estatico

--- src/main/java/com/unla/TPObjetosII/entities/SolicitudStock.java
+++ src/main/java/com/unla/TPObjetosII/entities/SolicitudStock.java
@@ -2,6 +2,7 @@ package com.unla.TPObjetosII.entities;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -35,8 +37,8 @@ public class SolicitudStock {
 	
 	
 	@JsonIgnore
-	@ManyToOne (fetch=FetchType.LAZY)
-	@JoinColumn(name="IDPEDIDO")
+	@OneToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name = "IDPEDIDO")
 	private Pedido pedido;
 	
 	@JsonIgnore
